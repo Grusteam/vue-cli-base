0021c0eccf7ec60bfa07cd8eacb6e90a005b7cf1
Serge Uniquite <sergeuniquite@gmail.com>
2018-12-11 13:23:23 +0300

    base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container">
+		{{ a }}
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return	{
+				a: 1
+			}
+		}
+	}
+</script>
+<style scoped>
+	.container {
+		color: red;
+	}
+</style>
