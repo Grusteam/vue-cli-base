2a0441b9b9500273bbd0a6d205df6f7c43f7c402
Serge Uniquite <sergeuniquite@gmail.com>
2018-12-11 16:07:49 +0300

    basic controls

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
 	<div class="container">
-		{{ a }}
+		<div class="wrapper">
+			<input type="text" v-model="test">
+			<div>{{ test }}</div>
+		</div>
+		<div class="wrapper">
+			<div v-show="show1()">show1</div>
+		</div>
+		<div class="wrapper">
+			<button
+				v-on:click="show2 = !show2"
+				v-bind:title="show2 ? 'Hide' : 'Show'"
+			>
+				__show2
+			</button>
+			
+			<div
+				class="wrapper"
+				v-show="show2"
+			>
+				show2__
+			</div>
+
+		</div>
+		<div class="wrapper">
+			<button
+				@click="add"
+			>
+				random
+			</button>
+			<ul class="list">
+				<li v-for="num in arr" class="list-item">{{ num }}</li>
+			</ul>
+		</div>
+		<div class="wrapper">
+			{{ seven }}
+		</div>
 	</div>
 </template>
 <script>
 	export default {
 		data() {
 			return	{
-				a: 1
+				test: 'default',
+				show2: true,
+				ten: 10,
+				arr: [],
 			}
-		}
+		},
+		computed: {
+			seven() {
+				console.log('seven');
+				return this.ten - 3
+			},
+		},
+		methods: {
+			show1: () => true,
+			add(e) {
+				const { target } = e;
+
+				console.log('target', target);
+				this.arr.push(Math.random())
+			},
+		},
 	}
 </script>
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import _ from 'lodash'
 
-console.log('_', _);
+// console.log('_', _);
 
 new Vue({
   el: '#app',
